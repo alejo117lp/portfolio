4e3f6fcac94f0a50b2412bc45ac79e6d8bf1484d
Alejandro López Pérez <alejo-197@outlook.com>
2024-12-30 08:15:45 -0500

    game features section structure

--- src/components/GameDetails/GameDetails.jsx
+++ src/components/GameDetails/GameDetails.jsx
@@ -4,7 +4,7 @@ import '../GameDetails/GameDetails.css';
 const GameDetails = (props) => {
   return(
     <div className="principal-container">
-      <h1 className="featured">{props.name}</h1>
+      <h1 className="featured mt-4">{props.name}</h1>
       <section className="game-summary">
         <iframe 
           className="game-trailer"
@@ -22,14 +22,27 @@ const GameDetails = (props) => {
           <a href={props.downloadLink} className="btn-a" target="_blank" rel="noreferrer">Descargar</a>
         </div>
       </section>
+
       <section className="game-features">
-
+        <div className="game-states">
+          <p><strong>Plataformas: </strong></p> {props.platforms}
+          <p><strong>Género: </strong></p> {props.genre}
+          <p><strong>Tags: </strong></p> {props.tags}
+          <p><strong>Estado: </strong></p> {props.status}
+        </div>
+        <div className="game-poster">
+          <img src={props.poster} alt="Poster del producto" />
+        </div>
       </section>
+
       <section className="task-table">
-
-      </section>
-      <section className="gallery-section">
+        <h3 className="feature-2">Tareas Desarrolladas:</h3>
         
+      </section>
+
+      <section className="gallery-section">
+        <h3 className="feature-2">Galería</h3>
+
       </section>
     </div>
   )
--- src/pages/Games/DeliveryJourney.jsx
+++ src/pages/Games/DeliveryJourney.jsx
@@ -9,7 +9,7 @@ const DeliveryJourney = () => {
       downloadLink = 'https://drive.google.com/file/d/1JyK5kubGs8Tu-MeuVVUDfxx5AxqlFruK/view?usp=sharing'
       description={
         <p>
-          Eres un repartidor de la empresa "EntregasD.una" y residente de Polombia , tu rol en la empresa es entregar distintos productos de distintas empresas en ciertos puntos de distribucion.
+          Eres un repartidor de la empresa "EntregasD.una" y residente de Polombia, tu rol en la empresa es entregar distintos productos de varias empresas en ciertos puntos de distribucion.
           Para escapar de este lúgubre lugar dominado por fuerzas malignas EntregasD.una le da el beneficio de trabajar en el exterior a sus mejores empleados. 
           Sabes lo que hay en las viejas y deplorables carreteras de este maligno país , sin embargo, tienes que demostrar que eres habilidoso a la hora de conducir para poder escapar de las fosas del mal.
         </p>
