a4b6e5954b90f21fe35ffd7a4a7e5aa9b2ae7f9e
Alejandro López Pérez <alejo-197@outlook.com>
2025-04-17 17:37:15 -0500

    Add a short descriptionto Portal Empleados

--- src/pages/PortalEmpleados/PortalEmpleados.jsx
+++ src/pages/PortalEmpleados/PortalEmpleados.jsx
@@ -9,11 +9,21 @@ const PortalEmpleados = () => {
     <div className="principal-container">
       <FrontendDetails
         title="Portal de Empleados"
+        description={
+          <>
+            Se desarrolló un portal web integral diseñado para optimizar la comunicación y la gestión de información interna de la <strong>Fundación Universidad de Antioquia</strong>.<br />
+            Esta solución incluyó funcionalidades como un formulario para la actualización de datos generales, consulta de colillas de pago, generación de certificados laborales y firma de documentos, entre otros.<br />
+            <br />
+            Mi rol fue <strong>diseñador</strong> y <strong>desarrollador frontend</strong>, utilizando <span style={{ fontWeight: 'bold' }}>Figma</span> para el diseño visual y <strong>React</strong> y <strong>Node.js</strong> para el desarrollo.<br />
+            Se aplicaron principios de diseño responsivo para garantizar una experiencia de usuario óptima en diversos dispositivos.
+          </>
+        }
         images={[
           { src: img1, alt: "Vista 1 del Portal de Empleados" },
           { src: img2, alt: "Vista 2 del Portal de Empleados" },
         ]}
       />
+
     </div>
   );
 };
