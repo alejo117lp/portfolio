25ee3ef8dd788324098593e20b35027acccf39d2
alejo117lp <alejo-197@outlook.com>
2025-08-06 14:03:25 -0500

    Fix navegation to portal empleados

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -91,6 +91,7 @@ const Home = () => {
               sus turnos de trabajo, vacaciones entre otros.
             </p>
           }
+          navigate="/portal-empleados"
         />
       </section>
 
